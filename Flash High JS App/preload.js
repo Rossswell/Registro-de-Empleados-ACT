5bbe560368eb7891162497e031652943598b6cac
Maxwell <santi200679@gmail.com>
2026-04-10 09:47:40 -0400

    c

--- Flash High JS App/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimize:    () => ipcRenderer.send('win-minimize'),
-  maximize:    () => ipcRenderer.send('win-maximize'),
-  close:       () => ipcRenderer.send('win-close'),
-  isMaximized: () => ipcRenderer.invoke('win-is-maximized'),
-})
